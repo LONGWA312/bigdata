cb6fd827546fbb24793b0753720c5529fb214754
jiye.li <1208640865@qq.com>
2023-06-04 00:44:13 +0800

    1

---
+++ study/d00_spark_demo/src/main/scala/com/study/demo/spark/other/Main.scala
@@ -0,0 +1,9 @@
+package com.study.demo.spark.other
+
+object Main {
+
+    def main(args: Array[String]): Unit = {
+        println("main")
+    }
+
+}
---
+++ study/d00_spark_demo/src/main/scala/com/study/demo/spark_sql/other/Main.scala
@@ -0,0 +1,9 @@
+package com.study.demo.spark_sql.other
+
+object Main {
+
+    def main(args: Array[String]): Unit = {
+        println("main")
+    }
+
+}
